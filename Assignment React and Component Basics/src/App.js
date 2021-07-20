3d3c174bf26bbbe03616cb3afe34ef13646e5881
Immanual <immanualatrinity@gmail.com>
2021-07-20 21:21:33 +0530

    code added

---
+++ Assignment React and Component Basics/src/App.js
@@ -0,0 +1,23 @@
+
+import Expense from "./components/Expense";
+
+function App() {
+
+	let expense = [
+		{ name: "Grocery", amt: 250.00, date: new Date(2021, 2, 28) },
+		{ name: "Fridge", amt: 12250.00, date: new Date(2019, 7, 12) },
+		{ name: "Washing machine", amt: 150.00, date: new Date(2020, 5, 18) }
+	];
+
+	return (
+		<div>
+			<h2>Let's get started!</h2>
+
+			{/* The below html property - expenseArray to access props in Expense.js */}
+			<Expense items={expense} />
+
+		</div>
+	);
+}
+
+export default App;
