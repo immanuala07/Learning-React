2662e449d1e77204d74d36be6e484e733c37b8a3
Immanual <immanualatrinity@gmail.com>
2021-07-22 22:57:01 +0530

    Code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import './ExpenseItem.css';
 import React from "react";
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 
 function ExpenseItem(props) {
 	return ( // The below html elements should have only one root element
