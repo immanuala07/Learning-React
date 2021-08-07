1b3a8b5c13c9a97f328583c29fbac166ced718de
Immanual <immanualatrinity@gmail.com>
2021-08-07 09:41:13 +0530

    comments added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,12 +3,22 @@ import React from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+
+	// An onChange event handler returns a Synthetic Event object 
+	// which contains useful meta data such as the target input’s id, name, and current value.
+	// We can access the target input’s value inside of the changeTitle by accessing e.target.value.
+	// 	Therefore, to log the name of the input field, we can log e.target.name.
+	const changeTitle = (event) => {
+		console.log(event.target.value);
+	};
+
 	return (
 		<form>
 			<div className='new-expense__controls'>
 				<div className='new-expense__control'>
 					<label>Title</label>
-					<input type='text' />
+					{/* onChange react event handler uses changeTitle funtion in this example will internally have event as the parameter */}
+					<input type='text' onChange={changeTitle} />
 				</div>
 				<div className='new-expense__control'>
 					<label>Amount</label>
