70b81e23ce2467c6c5a54d5f283c6a2494556a16
Immanual <immanualatrinity@gmail.com>
2021-09-07 10:10:12 +0530

    Using Stateful list - code

--- src/App.js
+++ src/App.js
@@ -2,30 +2,33 @@ import React, { useState } from "react";
 import Expense from "./components/Expenses/Expense";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+let intialExpense = [
+	{ name: "Coffee Maker", amt: 199.00, date: new Date(2022, 5, 18) },
+	{ name: "Fridge", amt: 12250.00, date: new Date(2019, 7, 12) },
+	{ name: "Washing machine", amt: 150.00, date: new Date(2020, 5, 18) },
+	{ name: "Grocery", amt: 250.00, date: new Date(2021, 2, 28) }
+];
+
 const App = () => { // Arrow function
-
-	let expense = [
-		{ name: "Coffee Maker", amt: 199.00, date: new Date(2022, 5, 18) },
-		{ name: "Fridge", amt: 12250.00, date: new Date(2019, 7, 12) },
-		{ name: "Washing machine", amt: 150.00, date: new Date(2020, 5, 18) },
-		{ name: "Grocery", amt: 250.00, date: new Date(2021, 2, 28) }
-	];
-
-	// Load the expense data with initial year as 2022 
-	// let initialExpenseData = expense.filter((item) => item.date.getFullYear().toString() == '2022');
-
-	const [expenseData, filteredExpenseData] = useState(expense); // eslint-disable-line no-unused-vars
+	// The below array destructing initialize the value and function to set the same value.
+	const [expenses, setExpenses] = useState(intialExpense);
 
 	const addExpenseHandler = expense => {
 		console.log('In App.js');
 		console.log(expense);
+
+		// The below function sets the value for state/data which is defined in useState (React function)
+		// setExpenses(expense);
+
+		// The below function calls inbuilt calls callback function whose parameter consists of previous value from the function defined in useState.
+		setExpenses((prevExpense) => {
+			return [expense, ...prevExpense];
+		});
 	};
 
 	const UpdatedYear = year => {
 		console.log('In App.js');
 		console.log(year);
-		// let expenseData = expense.filter((item) => item.date.getFullYear() == year);
-		// filteredExpenseData(expenseData);
 	};
 
 	return (
@@ -36,7 +39,7 @@ const App = () => { // Arrow function
 			<NewExpense onAddExpense={addExpenseHandler} />
 
 			{/* The below html property - expenseArray to access props in Expense.js */}
-			<Expense onSelectedYear={UpdatedYear} items={expenseData} />
+			<Expense onSelectedYear={UpdatedYear} items={expenses} />
 		</div>
 	);
 
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,9 @@
-// import React from "react";
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-
-	// useState - react hooks
-	// It is a function providedby the react library
-	// allows to define values as state where chnages to these values
-	// should reflect in the component function when being called again
-	const [title, setTitle] = useState(props.itemName); // eslint-disable-line no-unused-vars
-	//The above state return an arry of two elemnts
-	// title (special variable)- Output or updated value to be used further in custom component
-	// setTitle (function) - Function to be used for updating the new title whhen new title is passed as parameter
-
-	const clickHandler = () => {
-		setTitle("Title Changed - Button clicked...!");
-	};
-
 	return ( // The below html elements should have only one root element
 		<Card className="expense-item">
 			{/* <ExpenseDate></ExpenseDate> */}
@@ -26,13 +11,9 @@ function ExpenseItem(props) {
 			<ExpenseDate purchaseDate={props.purchaseDate} />
 			<div className="expense-item__description">
 				{/* The special variable from line number 12 is used below to display on the webpage */}
-				<h2>{title}</h2>
+				<h2>{props.itemName}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
-			{/* CLickHander is mentioned below as varaibale but not as function because if it mentioned as function 
-			it will be called when button in initialized on the dom instead calling when button is clicked
-			So when we mention it as varaible then this function is called on the button click */}
-			<button onClick={clickHandler}>Change Title</button>
 		</Card>
 	);
 }
