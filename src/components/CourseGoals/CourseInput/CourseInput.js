d30a48c04654be28811b382d0ae2ac1d37e6e0db
Immanual <immanualatrinity@gmail.com>
2021-10-19 08:49:17 +0530

    Styled components & dynamic props

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,9 +1,36 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
 
-const CourseInput = props => {
+const FormControl = styled.div`
+margin: 0.5rem 0;
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+	color: ${props => (props.invalid ? 'red' : 'black')};
+}
+
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+  background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: #fad0ec;
+	border-color: #8b005d;
+}
+`;
+
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
   /* useState is used to set dynamic inline styles if the inputbox is empty */
   const [isValid, setIsValid] = useState(true);
@@ -27,11 +54,22 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      {/* Instead of adding styles dynamic to individual html tags we are adding styles to div tag so the styles are added to the tags within the div tag */}
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+			<FormControl invalid={!isValid}>
+				<label>Course Goal</label>
+				<input type="text" onChange={goalInputChangeHandler} />
+			</FormControl>
+
+			{/* Created the FormControl component which has className - checks for the !isValid variable is undefined or not returns ' invalid' */}
+			{/* <FormControl className={!isValid && ' invalid'}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl> */}
+
+      {/* Instead of adding styles dynamic to individual html tags we are adding styles to div tag so the styles are added to the tags within the div tag */}
+			{/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div> */}
       <Button type="submit">Add Goal</Button>
     </form>
   );
