d332509528201728043cc3be5686ff8cfd80a289
Immanual <immanualatrinity@gmail.com>
2021-10-25 00:18:16 +0530

    using css modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,44 +1,46 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
-import styled from 'styled-components';
+import styles from "./Button.module.css";
 
 // Adding the button styles using styled component package  
-const Button = styled.button`
-	// button size is 100% till the maximum width of the page is 769px.
-	width: 100%;
+// const Button = styled.button`
+// 	// button size is 100% till the maximum width of the page is 769px.
+// 	width: 100%;
 
-	font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// 	font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-// Media queries - Handles button size to be auto till the minimum width of the page is 768px.
-@media (min-width: 768px) {
-	width: auto;
-}
+// // Media queries - Handles button size to be auto till the minimum width of the page is 768px.
+// @media (min-width: 768px) {
+// 	width: auto;
+// }
 
-&:focus {
-  outline: none;
-}
+// &:focus {
+//   outline: none;
+// }
 
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}`;
+// &:hover,
+// &:active {
+//   background: #ac0e77;
+//   border-color: #ac0e77;
+//   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+// }`;
 
 // The below button is not used instead of that the above styled component is used for button.
 // Button type, and classname are applied inbuilt through styled component.
 
-// const Button = props => {
-// 	return (
-// 		<button type={props.type} className="button" onClick={props.onClick}>
-// 			{props.children}
-// 		</button>
-// 	);
-// };
+const Button = props => {
+	return (
+		// Instead of applying a string className we refer to the dynamic - "styles object" in importing from the css file and refer the styles to the button property
+		// <button type={props.type} className="button" onClick={props.onClick}>
+		<button type={props.type} className={styles.button} onClick={props.onClick}>
+			{props.children}
+		</button>
+	);
+};
 
 export default Button;
