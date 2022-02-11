24d9d41c5ae2f1ce446e765e2965b53b8ad9441b
Immanual <immanualatrinity@gmail.com>
2022-02-11 09:37:38 +0530

    added useEffect hook code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,26 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+	/*
+	useEffect - It is hook allows you to perform side effects in the components.
+	Examples - Fetching data, directly updating the DOM, and timers.
+	*/
+	useEffect(() => {
+		const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+		if (storedUserLoggedInInformation) {
+			setIsLoggedIn(true);
+		}
+	}, []);
+
   const loginHandler = (email, password) => {
+		localStorage.setItem('isLoggedIn', '1');
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+		localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
