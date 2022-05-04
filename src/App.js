a8f253bd3492fcd9881047b7ea4992a0b68c7c29
Immanual <immanualatrinity@gmail.com>
2022-05-04 09:05:01 +0530

    Adding Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React, { Fragment } from "react";
+// Its ok to omit the above React import in modern React projects
+// you can always add it as well though.
+// We can always import React from React since it's required 
+// for the JSX is transformation at behind the scenes
+
+import Header from "./components/Layout/Header";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
