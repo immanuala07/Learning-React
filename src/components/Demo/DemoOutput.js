c7ab3f59931c3ac11f11d228b9e96f16476ecd50
Immanual <immanualatrinity@gmail.com>
2022-05-23 09:13:00 +0530

    Avoid-unnecessary-Re-Evaluation-using-React-Memo

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -2,8 +2,10 @@ import React from 'react';
 import MyParagraph from './MyParagraph';
 
 const DemoOutput = props => {
-    console.log('DemoOutput Ru nning')
+    console.log('DemoOutput Running')
     return <MyParagraph>{props.show ? 'This is new!' : ''}</MyParagraph>;
 };
 
-export default DemoOutput;
+// Using memo will cause React to skip rendering a component if its props have not changed.
+// This can improve performance.
+export default React.memo(DemoOutput);
