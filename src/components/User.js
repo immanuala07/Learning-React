9e90245e33c58d4cf4f4a00f41fdad3257838c31
Immanual <immanualatrinity@gmail.com>
2022-05-31 08:53:37 +0530

    Adding first class based component

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,34 @@
+import {Component} from 'react';
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+//--------- Class based components ---------
+// JS class is not an object and it is a template for JS objects.
+class User extends Component {
+
+  /*
+  The constructor method is a special method:
+  It has to have the exact name "constructor"
+  It is executed automatically when a new object is created
+  It is used to initialize object properties
+  The constructor method is called automatically when a new object is created.
+  If you do not define a constructor method, JavaScript will add an empty constructor method.
+  constructor() {}
+  */
+
+  // React renders HTML to the web page by using a function called render().
+  // Render function is to display the specified HTML code inside the specified HTML element.
+  // Render() method, we can read props and state and return our JSX code to the root component of our app.
+  // Render() method, we cannot change the state, and we cannot cause side effects(such as making an HTTP request to the webserver).
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// (or)
+
+//--------- Functional based components ---------
+// const User = (props) => {
+//   return <li className={classes.user}>{this.props.name}</li>;
+// };
 
 export default User;
