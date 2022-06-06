c64a160824f07b0f991b94ea11111a14dac8ec71
Immanual <immanualatrinity@gmail.com>
2022-06-06 16:54:04 +0530

    Class Component working with state and props

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,29 +9,60 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
+class Users extends Component {
+  /*
+  The constructor method is a special method:
+  It has to have the exact name "constructor"
+  It is executed automatically when a new object is created
+  It is used to initialize object properties
+  The constructor method is called automatically when a new object is created.
+  If you do not define a constructor method, JavaScript will add an empty constructor method.
+  */
+  constructor() {
+    // The super keyword is used to call the constructor of its parent class
+    // to access the parent's properties and methods.
+    super();
+    this.state = {
+      showUsers: true,
+    };
+  }
 
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
+  toggleUsersHandler() {
+    // To set the state value we cannot use th below statement
+    // this.state.showUsers = false;
 
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
+    // Instead of using the above method we can use the below way of setting the state Value
 
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+    // To change a value in the state object, use the this.setState() method.
+    // When a value in the state object changes, the component will re-render,
+    // meaning that the output will change according to the new value(s).
+    this.setState((curState) => {
+      // If the state value is dependent on the last value then use this functuon form
+      return { showUsers: !curState.showUsers };
+    });
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        {/* By using javscript bind() function, we are making the toggleUsersHandler function to use the users object 
+        instead of using the Button as object. */}
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {/* Using the state value */}
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
 
 export default Users;
