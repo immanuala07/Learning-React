f062aa4813144bdec2397224fb741eff052d66c5
Immanual <immanualatrinity@gmail.com>
2022-06-14 21:11:57 +0530

    Using async and await for Fetch API

--- src/App.js
+++ src/App.js
@@ -6,24 +6,22 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
+  async function fetchMoviesHandler() {
     // Fetch() method that allows you to fetch data from all sorts of different places and work with the data fetched.
     // It allows you to make an HTTP request, i.e., either a GET request (for getting data) or POST request (for posting data).
-    fetch('https://swapi.dev/api/films').then(response => {
-      return response.json();
-    }).then((data) => {
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          rreleaseDate: movieData.release_date
-        };
-      });
-      console.log('Data is retrieved');
-      setMovies(transformedMovies);
-    })
-  }
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        rreleaseDate: movieData.release_date
+      };
+    });
+    console.log('Data is retrieved');
+    setMovies(transformedMovies);
+  };
 
   return (
     <React.Fragment>
