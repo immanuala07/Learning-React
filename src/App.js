ce8e01fe963ddf3d147c5abdd63fcef319c023d7
Immanual <immanualatrinity@gmail.com>
2022-06-16 19:28:44 +0530

    Sending POST request using fetch api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -17,20 +18,24 @@ function App() {
     try {
       // Fetch() method that allows you to fetch data from all sorts of different places and work with the data fetched.
       // It allows you to make an HTTP request, i.e., either a GET request (for getting data) or POST request (for posting data).
-      const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch('https://react-http-1e116-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
       const data = await response.json();
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      });
-      setMovies(transformedMovies);
+
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -46,6 +51,23 @@ function App() {
     // removes unwanted infinite calls of the below function.
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    // The Fetch API is a promise-based interface for fetching resources by making HTTP requests to servers from web browsers.
+    // It is similar to XML HTTP requests but better and more powerful.
+    // method – HTTP-method, e.g. POST, GET and so on.
+    // body – the request body or content.
+    // json(): it resolves promise with JSON.
+    const response = await fetch(`https://react-http-1e116-default-rtdb.firebaseio.com/movies.json`, {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+  }
 
   let content = <p>Found no movies.</p>;
 
@@ -63,6 +85,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
