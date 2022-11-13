0de3995ff1bca84799988666ce81a7b540d34bf5
Immanual <immanualatrinity@gmail.com>
2022-11-13 08:16:06 +0530

    Dealing With Form Submission & Getting User Input Values

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,47 @@
-const SimpleInput = (props) => {
+import {useRef, useState} from 'react';
+
+const SimpleInput = ( props ) => {
+  // Use the value only once after the form is submitted
+  const nameInputRef = useRef();
+
+  // Use the value for every keystroke on name textfield
+  const [ enteredName, setEnteredName ] = useState( '' );
+
+  const nameInputChangeHandler = ( event ) => {
+    setEnteredName( event.target.value );
+  };
+
+  const formSubmissionHandler = ( event ) => {
+    event.preventDefault();
+
+    console.log( enteredName );
+
+    const enteredValue = nameInputRef.current.value;
+    console.log( enteredValue );
+
+    // nameInputRef.current.value = '';  => NOT IDEAL, DONT MANIPULATE THE DOM
+
+    // Two way binding
+    setEnteredName( '' );
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          // Use the value for every keystroke on name textfield
+          onChange={nameInputChangeHandler}
+
+          ref={nameInputRef}
+
+          // Two way binding
+          value={enteredName}
+        />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
