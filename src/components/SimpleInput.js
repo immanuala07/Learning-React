1419a3d996fe324fb81c5109f19a8dfb959316a1
Immanual <immanualatrinity@gmail.com>
2022-11-13 14:10:55 +0530

    Providing Validation Feedback

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -7,6 +7,8 @@ const SimpleInput = ( props ) => {
   // Use the value for every keystroke on name textfield
   const [ enteredName, setEnteredName ] = useState( '' );
 
+  const [ enteredNameIsValid, setEnteredNameIsValid ] = useState( true );
+
   const nameInputChangeHandler = ( event ) => {
     setEnteredName( event.target.value );
   };
@@ -16,9 +18,11 @@ const SimpleInput = ( props ) => {
 
     if ( enteredName.trim() === '' )
     {
+      setEnteredNameIsValid( false );
       return;
     }
 
+    setEnteredNameIsValid( true );
     console.log( enteredName );
 
     const enteredValue = nameInputRef.current.value;
@@ -30,9 +34,11 @@ const SimpleInput = ( props ) => {
     setEnteredName( '' );
   };
 
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input
           type='text'
@@ -45,6 +51,7 @@ const SimpleInput = ( props ) => {
           // Two way binding
           value={enteredName}
         />
+        {!enteredNameIsValid && <p className='error-text'>Name must not be empty.</p>}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
