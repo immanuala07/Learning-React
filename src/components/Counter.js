331424942e9b1f76515a5bab24bc3e0bb85e5fc9
Immanual <immanualatrinity@gmail.com>
2022-11-27 21:54:53 +0530

    Using Redux Data in React Components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,30 @@
+import { useSelector } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  /*
+  const result: any = useSelector(selector: Function, equalityFn?: Function)
+  
+  Allows you to extract data from the Redux store state, using a selector function.
+
+  The selector will be called with the entire Redux store state as its only argument. The selector will be run whenever the function component renders
+  (unless its reference hasn't changed since a previous render of the component so that a cached result can be returned by the hook without re-running the selector).
+  useSelector() will also subscribe to the Redux store, and run your selector whenever an action is dispatched.
+
+  We may call useSelector() multiple times within a single function component.
+  Each call to useSelector() creates an individual subscription to the Redux store.
+  Because of the React update batching behavior used in React Redux v7,
+  a dispatched action that causes multiple useSelector()s in the same component to return new values should only result in a single re-render.
+  */
+  const counter = useSelector(state => state.counter);
+
+  const toggleCounterHandler = () => { };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
