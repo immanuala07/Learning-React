1474f4d8d284f6f3a73d9a4286bf69993d9ba647
Immanual <immanualatrinity@gmail.com>
2022-12-01 21:47:13 +0530

    Redux with Class-based Components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,7 @@
-import { useSelector, useDispatch } from 'react-redux';
-
 import classes from './Counter.module.css';
+
+/*
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
 
@@ -31,7 +32,7 @@ const Counter = () => {
   Each call to useSelector() creates an individual subscription to the Redux store.
   Because of the React update batching behavior used in React Redux v7,
   a dispatched action that causes multiple useSelector()s in the same component to return new values should only result in a single re-render.
-  */
+
   const counter = useSelector(state => state.counter);
 
   const toggleCounterHandler = () => { };
@@ -50,3 +51,72 @@ const Counter = () => {
 };
 
 export default Counter;
+*/
+
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Counter extends Component {
+
+  incrementHandler () {
+    this.props.increment();
+  }
+
+  decrementHandler () {
+    this.props.decrement();
+  }
+
+  toggleCounterHandler () { }
+
+  render () {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{this.props.counter}</div>
+        <div>
+          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+        </div>
+        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
+
+// This function is equivalent to useSelector() hook of functional components in react.
+// Hooks are not unable in class based components.
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter
+  }
+};
+
+// This function is equivalent to useDispatch() hook of functional components in react.
+// Hooks are not unable in class based components.
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // dispatch is a function which is in variable form.
+    increment: () => dispatch({ type: 'increment' }),
+    decrement: () => dispatch({ type: 'decrement' })
+  };
+};
+
+/*
+The connect() function connects a React component to a Redux store.
+
+It provides its connected component with the pieces of the data it needs from the store,
+and the functions it can use to dispatch actions to the store.
+
+It does not modify the component class passed to it;
+instead, it returns a new, connected component class that wraps the component you passed in.
+
+function connect(mapStateToProps?, mapDispatchToProps?, mergeProps?, options?)
+
+The mapStateToProps and mapDispatchToProps deals with your Redux store’s state and dispatch, respectively.
+state and dispatch will be supplied to your mapStateToProps or mapDispatchToProps functions as the first argument.
+
+The returns of mapStateToProps and mapDispatchToProps are referred to internally as stateProps and dispatchProps, respectively.
+They will be supplied to mergeProps, if defined, as the first and the second argument, where the third argument will be ownProps.
+The combined result, commonly referred to as mergedProps, will then be supplied to your connected component.
+*/
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
