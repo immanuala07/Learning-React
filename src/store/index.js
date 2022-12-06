b063ae212bebf8ed8e22c62c86d261dbba7d851a
Immanual <immanualatrinity@gmail.com>
2022-12-06 09:48:56 +0530

    Connecting Redux Toolkit State

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 // In an application there can be only one central data store and there cannot be more than one central data store.
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+// import { createStore } from 'redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   counter: 0,
@@ -12,7 +12,7 @@ const initialState = {
  and automatically generates action creators and action types that correspond to the reducers and state.
  The reducer argument is passed to createReducer().
 */
-createSlice({
+const counterSlice = createSlice({
   // Name of the slice
   name: 'counter',
   // The initial state for the reducer
@@ -37,50 +37,28 @@ createSlice({
   }
 });
 
-/*
-Reducer function will produce new state snapshots.
-The reducer function has to go of spitting out a new state snapshot whenever an action reaches it.
-Reducer functions is a pure function i.e; same input always should produce exactly the same output.
-There is no side effects inside of that function.
-We must not send a http request or write something to local storage or fetch something from local storage.
+/* 
+Create a new store and also point the reducer function as variable in the parameter.
+But if we have bigger applications with multiple state slices,we would face a problem
+because there can only be one reducer passed to createStore().
 
-state - Previous state value
-action - Current or latest state value
+When we have multiple slices, we have multiple reducers which we access with .reducer on the different slices.
 */
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 1,
-      showCounter: true
-    }
-  }
+// const store = createStore(counterReducer);
 
-  if (action.type === 'increaseBy5') {
-    return {
-      // Added payload to Action with action.amount (to fetch latest amount value) for generic approach
-      counter: state.counter + action.amount,
-      showCounter: true
-    }
-  }
+/*
+A friendly abstraction over the standard Redux createStore() function.
+ConfigureStore like createStore creates a store but it makes merging multiple reducers
+into one reducer easier.
 
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: true
-    }
-  }
-
-  if (action.type === 'toggle') {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter
-    }
-  }
-
-  return state;
-};
-
-// Create a new store and also point the reducer function as variable in the parameter.
-const store = createStore(counterReducer);
+In configureStore(), we can pass an object not a reducer function but an object.
+It's a configuration object expected by configureStore.
+A configuration object where we then set a reducer property is
+an expected property by configureStore().
+Redux wants one main reducer function, which is responsible for the global state.
+*/
+const store = configureStore({
+  reducer: counterSlice.reducer
+});
 
 export default store;
