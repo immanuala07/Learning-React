0efe68183aeaad09fde29b85b8d2eba3521f6c18
Immanual <immanualatrinity@gmail.com>
2022-12-30 15:10:17 +0530

    React_Router-Defining-And-Using-Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-function App() {
+import { Route } from "react-router-dom";
+
+import Welcome from './components/Welcome';
+import Products from './components/Product';
+
+function App () {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      {/*
+      Route is a component that allows us to define a certain path,
+      and then the React component that should be loaded
+      when that path becomes active in the URL.
+      
+      Path prop in <Route><Route/>: The path pattern to match against the URL to determine
+      if this route matches a URL, link href, or form action.
+      */}
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+
+      {/*
+      Route is a component that allows us to define a certain path,
+      and then the React component that should be loaded
+      when that path becomes active in the URL.
+      
+      Path prop in <Route><Route/>: The path pattern to match against the URL to determine
+      if this route matches a URL, link href, or form action.
+      */}
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    /*
+    A <BrowserRouter> stores the current location in the browser's address bar 
+    using clean URLs and navigates using the browser's built-in history stack.
+    */
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
