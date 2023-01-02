48583f29402ca28d300c5bbb6c3d3b0d862205c8
Immanual <immanualatrinity@gmail.com>
2023-01-02 18:15:47 +0530

    Working with Nested Routes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,18 @@
+import { Route } from 'react-router-dom';
+
 const Welcome = () => {
-    return <h1>The Welcome page</h1>
+	return (
+		<section>
+			<h1>The Welcome page</h1>
+			{/* 
+            Nested <Route> - We can define a <Route> within another <Route>.
+            We can define routes in other components in other routes therefore,
+            */}
+			<Route path="/welcome/new-user">
+				<p>Welcome, New User!</p>
+			</Route>
+		</section>
+	);
 };
 
 export default Welcome;
