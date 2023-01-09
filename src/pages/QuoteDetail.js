d3e3d9c63cc0fa679e94fac0391b5e3bc5bd1f6a
Immanual <immanualatrinity@gmail.com>
2023-01-09 18:28:57 +0530

    Getting Creative With Nested Routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { Route, useParams } from 'react-router-dom';
+import { Link, Route, useParams } from 'react-router-dom';
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
@@ -22,6 +22,19 @@ const QuoteDetail = () => {
 		// React Fragment - <></>
 		<Fragment>
 			<HighlightedQuote text={quote.text} author={quote.author} />
+
+			{/* Below Route component is used to hide the load Comments link once after it is clicked */}
+			<Route path="/quotes/:quoteId" exact>
+				<div className="centered">
+					<Link
+						className="btn--flat"
+						to={`/quotes/${params.quoteId}/comments`}
+					>
+						Load Comments
+					</Link>
+				</div>
+			</Route>
+
 			<Route path={`/quotes/${params.quoteId}/comments`}>
 				<Comments />
 			</Route>
