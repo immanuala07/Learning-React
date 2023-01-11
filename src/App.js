be2bbfc9f3a7aa679234e13e3706c8f3a0570c1f
Immanual <immanualatrinity@gmail.com>
2023-01-11 18:49:57 +0530

    Fixed indentation

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
 			<Switch>
 				<Route path="/" exact>
 					{/*
-                    Rendering a <Redirect> will navigate to a new location.
-                    
-                    The new location will override the current location in the history stack,
-                    like server-side redirects (HTTP 3xx) do.
-                    */}
+					Rendering a <Redirect> will navigate to a new location.
+
+					The new location will override the current location in the history stack,
+					like server-side redirects (HTTP 3xx) do.
+					*/}
 					<Redirect to="/quotes" />
 				</Route>
 				<Route path="/quotes" exact>
@@ -29,16 +29,16 @@ function App() {
 					<NewQuote />
 				</Route>
 				{/* 
-                The '*' wildcard character signals to react router
-                that any path any URL should match this route,
-                and therefore this route has to come last.
-                
-                So that it does not consume one of the requests
-                to one of the actual routes we have.
-                But if we didn't have any match up to this point
-                then we want to match all URLs with this route.
-                And then just rendered this not found page here.
-                */}
+				The '*' wildcard character signals to react router
+				that any path any URL should match this route,
+				and therefore this route has to come last.
+
+				So that it does not consume one of the requests
+				to one of the actual routes we have.
+				But if we didn't have any match up to this point
+				then we want to match all URLs with this route.
+				And then just rendered this not found page here.
+				*/}
 				<Route path="*">
 					<NotFound />
 				</Route>
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,43 +1,44 @@
 import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
 	/*
-    The useHistory hook gives you access to the history instance
-    that you may use to navigate.
+	The useHistory hook gives you access to the history instance
+	that you may use to navigate.
 
-    useHistory() - This hook gives the access to the history instance or objects used by React Router.
-    Using the history instance you can redirect users to another page.
-    The history instance created by React Router uses a Stack( called “History Stack” ),
-    that stores all the entries the user has visited.
+	useHistory() - This hook gives the access to the history instance or objects used by React Router.
+	Using the history instance you can redirect users to another page.
+	The history instance created by React Router uses a Stack( called “History Stack” ),
+	that stores all the entries the user has visited.
 
-    History objects typically have the following properties and methods:
-    1) push(path, [state]) - (function) Pushes a new entry onto the history stack and
-    it allows the user to go back to the previous page.
-    It is useful to redirect users to page.
-    2) replace(path, [state]) - (function) Replaces the current entry on the history stack
-    and it doesn't allows the user to go back to the previous page.
-    */
+	History objects typically have the following properties and methods:
+	1) push(path, [state]) - (function) Pushes a new entry onto the history stack and
+	it allows the user to go back to the previous page.
+	It is useful to redirect users to page.
+	2) replace(path, [state]) - (function) Replaces the current entry on the history stack
+	and it doesn't allows the user to go back to the previous page.
+  */
 	const history = useHistory();
 
 	const addQuoteHandler = (quoteData) => {
 		console.log(quoteData);
 		/*
-        The useHistory hook gives you access to the history instance
-        that you may use to navigate.
+		The useHistory hook gives you access to the history instance
+		that you may use to navigate.
 
-        useHistory() - This hook gives the access to the history instance or objects used by React Router.
-        Using the history instance you can redirect users to another page.
-        The history instance created by React Router uses a Stack( called “History Stack” ),
-        that stores all the entries the user has visited.
+		useHistory() - This hook gives the access to the history instance or objects used by React Router.
+		Using the history instance you can redirect users to another page.
+		The history instance created by React Router uses a Stack( called “History Stack” ),
+		that stores all the entries the user has visited.
 
-        History objects typically have the following properties and methods:
-        1) push(path, [state]) - (function) Pushes a new entry onto the history stack and
-        it allows the user to go back to the previous page.
-        It is useful to redirect users to page.
-        2) replace(path, [state]) - (function) Replaces the current entry on the history stack
-        and it doesn't allows the user to go back to the previous page.
-        */
+		History objects typically have the following properties and methods:
+		1) push(path, [state]) - (function) Pushes a new entry onto the history stack and
+		it allows the user to go back to the previous page.
+		It is useful to redirect users to page.
+		2) replace(path, [state]) - (function) Replaces the current entry on the history stack
+		and it doesn't allows the user to go back to the previous page.
+		*/
 		history.push('/quotes'); // After submitting the new-quote then the user is redirected to the AllQuotes page.
 	};
 
