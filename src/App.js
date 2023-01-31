9ef496c28a8d4a769b8a019b00285fe02f89f18b
Immanual <immanualatrinity@gmail.com>
2023-01-31 09:25:17 +0530

    Defining Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './pages/Home';
+
+/*
+createBrowserRouter() - This is the recommended router for all React Router web projects.
+It uses the DOM History API to update the URL and manage the history stack.
+
+It also enables the v6.4 data APIs like loaders, actions, fetchers and more.
+It also takes an array of route objects as parameter.
+*/
+const router = createBrowserRouter([
+	{ path: '/', element: <HomePage /> }
+]);
+
 function App() {
-  return <div></div>;
+	/*
+	A <BrowserRouter> stores the current location in the browser's address bar using clean URLs
+	and navigates using the browser's built-in history stack.
+	
+	<BrowserRouter window> defaults to using the current document's defaultView,
+	but it may also be used to track changes to another window's URL, in an <iframe>
+	*/
+	return <RouterProvider router={router} />;
 }
 
 export default App;
