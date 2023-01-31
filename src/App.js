806ff21fa3208a1aeccc6119659d39aefffb5c01
Immanual <immanualatrinity@gmail.com>
2023-01-31 21:48:12 +0530

    Exploring an Alternative Way of Defining Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider
+} from 'react-router-dom';
 
 import HomePage from './pages/Home';
 import ProductPage from './pages/Product';
+
+/*
+createRoutesFromElements() is a helper that creates route objects from <Route> elements.
+It's useful if you prefer to create your routes as JSX instead of objects.
+
+It's also used internally by <Routes> to generate a route objects from its <Route> children.
+*/
+const routerDefinitions = createRoutesFromElements(
+	<Route>
+		<Route path="/" element={<HomePage />} />
+		<Route path="/products" element={<ProductPage />} />
+	</Route>
+);
 
 /*
 createBrowserRouter() - This is the recommended router for all React Router web projects.
@@ -10,10 +28,14 @@ It uses the DOM History API to update the URL and manage the history stack.
 It also enables the v6.4 data APIs like loaders, actions, fetchers and more.
 It also takes an array of route objects as parameter.
 */
+const router = createBrowserRouter(routerDefinitions);
+
+/*
 const router = createBrowserRouter([
 	{ path: '/', element: <HomePage /> },
 	{ path: '/products', element: <ProductPage /> }
 ]);
+*/
 
 function App() {
 	/*
