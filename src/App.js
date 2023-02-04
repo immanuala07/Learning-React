83fdb4a1180f8e21955d3893d0b9b43a0ab70594
Immanual <immanualatrinity@gmail.com>
2023-02-04 23:36:08 +0530

    Layouts & Nested Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import {
 	createBrowserRouter,
+	/*
 	createRoutesFromElements,
 	Route,
+	*/
 	RouterProvider
 } from 'react-router-dom';
 
 import HomePage from './pages/Home';
 import ProductPage from './pages/Product';
+import RootLayout from './pages/Root';
 
 /*
 createRoutesFromElements() is a helper that creates route objects from <Route> elements.
 It's useful if you prefer to create your routes as JSX instead of objects.
 
 It's also used internally by <Routes> to generate a route objects from its <Route> children.
-*/
+
 const routerDefinitions = createRoutesFromElements(
 	<Route>
 		<Route path="/" element={<HomePage />} />
 		<Route path="/products" element={<ProductPage />} />
 	</Route>
 );
+*/
 
 /*
 createBrowserRouter() - This is the recommended router for all React Router web projects.
@@ -28,14 +32,17 @@ It uses the DOM History API to update the URL and manage the history stack.
 It also enables the v6.4 data APIs like loaders, actions, fetchers and more.
 It also takes an array of route objects as parameter.
 */
-const router = createBrowserRouter(routerDefinitions);
-
-/*
 const router = createBrowserRouter([
-	{ path: '/', element: <HomePage /> },
-	{ path: '/products', element: <ProductPage /> }
+	{
+		path: '/',
+		element: <RootLayout />,
+		children: [
+			{ path: '/', element: <HomePage /> },
+			{ path: '/products', element: <ProductPage /> }
+		]
+	}
 ]);
-*/
+
 
 function App() {
 	/*
