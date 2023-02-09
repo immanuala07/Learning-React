859fbde2646ceaed2affdbac56541f77db3f8d2f
Immanual <immanualatrinity@gmail.com>
2023-02-09 10:39:48 +0530

    Working-with-Navigation-Links-or-NavLink

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 
@@ -8,10 +8,67 @@ function MainNavigation() {
 			<nav>
 				<ul className={classes.list}>
 					<li>
-						<Link to="/">Home</Link>
+						<NavLink
+							to="/"
+							
+							/*
+							className can either be a string or a function that returns a string.
+							If the function className is used, the link’s active state is passed as a parameter.
+							This is helpful if you want to exclusively apply a className to an inactive link.
+
+							In React Router v6, activeClassName will be removed and
+							you should use the function className to apply classnames to either active or
+							inactive NavLink components.
+							*/
+							className={({ isActive }) =>
+								isActive ? classes.active : undefined
+							}
+							
+							/*
+							'end' prop ensures this component isn't matched as "active"
+							when its descendant paths are matched.
+							
+							For example, to render a link
+							that is only active at the website root and not any other URLs,
+							*/
+							end
+
+							/*
+							// Inline styles for NavLink
+							style={({ isActive }) => ({
+							 	textAlign: isActive ? 'center' : 'left'
+							})
+							*/
+						>
+							Home
+						</NavLink>
 					</li>
 					<li>
-						<Link to="/products">Products</Link>
+						<NavLink
+							to="/products"
+							
+							/*
+							className can either be a string or a function that returns a string.
+							If the function className is used, the link’s active state is passed as a parameter.
+							This is helpful if you want to exclusively apply a className to an inactive link.
+
+							In React Router v6, activeClassName will be removed and
+							you should use the function className to apply classnames to either active or
+							inactive NavLink components.
+							*/
+							className={({ isActive }) =>
+								isActive ? classes.active : undefined
+							}
+
+							/*
+							// Inline styles for NavLink
+							style={({ isActive }) => ({
+							 	textAlign: isActive ? 'center' : 'left'
+							})
+							*/
+						>
+							Products
+						</NavLink>
 					</li>
 				</ul>
 			</nav>
