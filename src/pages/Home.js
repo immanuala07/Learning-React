36178c91b90052391934353becd15646fa831d6d
Immanual <immanualatrinity@gmail.com>
2023-02-11 09:40:48 +0530

    Navigating Programmatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,28 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-function HomePage() {
+function HomePage () {
+	/*
+	The useNavigate hook returns a function that lets you navigate programmatically
+
+	The navigate function has two signatures:
+	a) Either pass a 'To' value (same type as <Link to>)
+	with an optional second { replace, state } arg or
+	b) Pass the delta you want to go in the history stack.
+	For example, navigate(-1) is equivalent to hitting the back button.
+	*/
+	const navigate = useNavigate();
+
+	function navigateHandler () {
+		navigate('/products');
+	}
+
 	return (
 		<>
 			<h1>My Home Page</h1>
 			<p>
 				Go to <Link to="/products">the list of products</Link>.
 			</p>
+			<button onClick={navigateHandler}>Navigate</button>
 		</>
 	);
 }
