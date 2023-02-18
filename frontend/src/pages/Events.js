30621c065f330bbaf0f17f10ad1f207e8ae5b86c
Immanual <immanualatrinity@gmail.com>
2023-02-18 18:06:23 +0530

    Using Data From A Loader In The Route Component

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,32 +1,37 @@
-import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-	const [isLoading, setIsLoading] = useState(false);
-	const [fetchedEvents, setFetchedEvents] = useState();
-	const [error, setError] = useState();
+	/*
+	This hook provides the value returned from your route loader.
 
-	useEffect(() => {
-		async function fetchEvents() {
-			setIsLoading(true);
+	The loader function in the route definition if it returns a promise.
+	React Router will actually check if a promise is returned
+	and automatically get the resolved data from that promise for you.
 
-			setIsLoading(false);
-		}
+	 
+	After route actions are called,
+	the data will be revalidated automatically
+	and return the latest result from your loader.
+	
+	Note that useLoaderData does not initiate a fetch.
+	It simply reads the result of a fetch React Router manages internally,
+	so you don't need to worry about it refetching
+	when it re-renders for reasons outside of routing.
+	
+	This also means data returned is stable between renders,
+	so you can safely pass it to dependency arrays in React hooks like useEffect.
+	It only changes when the loader is called again after actions or certain navigations.
+	In these cases the identity will change (even if the values don't).
+	
+	You can use this hook in any component or any custom hook,
+	not just the Route element.
+	It will return the data from the nearest route on context.
+	*/
+	const events = useLoaderData();
 
-		fetchEvents();
-	}, []);
-	return (
-		<>
-			<div style={{ textAlign: 'center' }}>
-				{isLoading && <p>Loading...</p>}
-				{error && <p>{error}</p>}
-			</div>
-			{!isLoading && fetchedEvents && (
-				<EventsList events={fetchedEvents} />
-			)}
-		</>
-	);
+	return <EventsList events={events} />;
 }
 
 export default EventsPage;
