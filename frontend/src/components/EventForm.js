fa5f84924b493b511a71e2a3c61e727eebed44a6
Immanual <immanualatrinity@gmail.com>
2023-03-05 13:26:49 +0530

    Updating-the-UI-State-Based-on-the-Submission-Status

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,9 +1,42 @@
-import { Form } from 'react-router-dom';
+import { Form, useNavigate, useNavigation } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
-function EventForm({ method, event }) {
-  function cancelHandler() {}
+function EventForm ({ method, event }) {
+  // The useNavigate hook returns a function that lets you navigate programmatically.
+  const navigate = useNavigate();
+
+  /*
+  useNavigation() - This hook tells you everything you need to know about a page navigation
+  to build pending navigation indicators and optimistic UI on data mutations.
+
+  Things like:
+  Global loading indicators
+  Disabling forms while a mutation is happening
+  Adding busy indicators to submit buttons
+  Optimistically showing a new record while it's being created on the server
+  Optimistically showing the new state of a record while it's being updated
+  */
+  const navigation = useNavigation();
+
+  /*
+  navigation.state
+  idle - There is no navigation pending.
+  submitting - A route action is being called due to a form submission using POST, PUT, PATCH, or DELETE
+  loading - The loaders for the next routes are being called to render the next page
+
+  Normal navigations and GET form submissions transition through these states:
+  idle → loading → idle
+
+  Form submissions with POST, PUT, PATCH, or DELETE transition through these states:
+  idle → submitting → loading → idle
+  */
+  const isSubmitting = navigation.state === "submitting";
+
+  function cancelHandler() {
+    // Below, we go back a step in history stack and is equivalent to hitting the back button.
+    navigate("..");
+  }
 
   return (
     /*
@@ -62,10 +95,16 @@ function EventForm({ method, event }) {
         />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button
+          type="button"
+          onClick={cancelHandler}
+          disabled={isSubmitting}
+        >
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Save"}
+        </button>
       </div>
     </Form>
   );
