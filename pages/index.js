d9573d9c1a9fd40adccb3e13fd4624cced0189ae
Immanual <immanualatrinity@gmail.com>
2023-03-23 10:20:43 +0530

    Adding first pages

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// our-domain.com/
+
+/*
+In nextJS project, react is internally imported so we can omit this import.
+import React from 'react';
+*/
+
+function HomePage() {
+  return <h1>The Home Page</h1>
+}
+
+export default HomePage;
---
+++ pages/news.js
@@ -0,0 +1,12 @@
+// our-domain.com/news
+
+/*
+In nextJS project, react is internally imported so we can omit this import.
+import React from 'react';
+*/
+
+function NewsPage () {
+  return <h1>The News Page</h1>
+}
+
+export default NewsPage;
