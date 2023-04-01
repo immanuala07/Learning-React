f225cac1a074af74438d5575d412a9fbd0c326f5
Immanual <immanualatrinity@gmail.com>
2023-04-01 21:41:35 +0530

    Adding Custom Components & CSS Modules

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,2 +1,15 @@
 // Dynamic page
 // our-domain.com/[any-value]
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails () {
+  return (
+    <MeetupDetail
+      title="A FIrst Meetup"
+      image="https://memberpress.com/wp-content/uploads/2019/10/Member-Meetup@2x.png"
+      address="Address1, city1"
+      description="This is first meetup!" />
+  );
+}
+
+export default MeetupDetails;
