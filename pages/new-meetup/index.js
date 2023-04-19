bc58a39db4a1a5d14b50710e78f2d837e8542e29
Immanual <immanualatrinity@gmail.com>
2023-04-19 07:54:56 +0530

    API_Routes_And_Working_with_MongoDB_And_Sending_Http_Requests_To_Our_API_Routes

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,62 @@
 // our-domain.com/new-meetup
+import { useRouter } from 'next/router';
 import NewMeetUpForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetUpPage () {
-  function addMeetUpHandler (enteredMeetUpData) {
-    console.log(enteredMeetUpData);
+  /*
+  useRouter is a React Hook, meaning it cannot be used with classes. You can either use withRouter or wrap your class in a function component.
+  If you want to access the router object inside any function component in your app, you can use the useRouter hook
+  The following is the definition of the router object returned by both useRouter and withRouter:
+  1) pathname: String - The path for current route file that comes after /pages.
+                        Therefore, basePath, locale and trailing slash (trailingSlash: true) are not included.
+  2) query: Object - The query string parsed to an object, including dynamic route parameters.
+                    It will be an empty object during prerendering if the page doesn't use Server-side Rendering.
+                    Defaults to {}.
+  3) asPath: String - The path as shown in the browser including the search params and respecting the trailingSlash configuration.
+                      basePath and locale are not included.
+  4) isFallback: boolean - Whether the current page is in fallback mode.
+  5) basePath: String - The active basePath (if enabled).
+  6) locale: String - The active locale (if enabled).
+  7) locales: String[] - All supported locales (if enabled).
+  8) defaultLocale: String - The current default locale (if enabled).
+  9) domainLocales: Array<{domain, defaultLocale, locales}> - Any configured domain locales.
+  10) isReady: boolean - Whether the router fields are updated client-side and ready for use.
+                        Should only be used inside of useEffect methods and not for conditionally rendering on the server.
+  11) isPreview: boolean - Whether the application is currently in preview mode.
+  */
+  const router = useRouter();
+
+  async function addMeetUpHandler(enteredMeetUpData) {
+		try {
+			/*
+      In this Next.js project, there is an internal API
+      which will be hosted by the same server for serving this page.
+      And therefore we can construct a absolute path to send the request
+      to the same server but a different path on that server.
+      
+      And the path is as given below:
+      because it's this special API folder and then the file name.
+      */
+			const response = await fetch("/api/new-meetup", {
+				method: "POST",
+				body: JSON.stringify(enteredMeetUpData),
+				headers: { "Content-Type": "application/json" }
+			});
+
+			const data = await response.json();
+
+      console.log(data);
+
+      /*
+      Similar to the replace prop in next/link, router.replace will prevent adding a new URL entry into the history stack.
+
+router.replace(url, as, options)
+The API for router.replace is exactly the same as the API for router.push.
+      */
+      router.replace('/');
+		} catch (error) {
+			console.log(error);
+		}
   }
 
   return <NewMeetUpForm onAddMeetup={addMeetUpHandler} />
