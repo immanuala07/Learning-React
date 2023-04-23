72f446b2822c2498176bca3ebdc7d468ad1634c7
Immanual <immanualatrinity@gmail.com>
2023-04-23 23:39:10 +0530

    Animating React Apps Demo Project

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import './Backdrop.css';
 
-const backdrop = (props) => (
-    <div className="Backdrop"></div>
-);
+const backdrop = (props) => {
+  console.log(props.show);
+  const cssClasses = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClosed'];
+  
+  // JS join() returns an array as a string.
+  return <div className={cssClasses.join(' ')}></div>
+};
 
 export default backdrop;
