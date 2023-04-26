89fa71c89bdc1efe5e42c9c8000d1ffc12f383c7
Immanual <immanualatrinity@gmail.com>
2023-04-26 23:50:08 +0530

    Using React Transition Group

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
+import { Transition } from "react-transition-group";
 
 class App extends Component {
   // Initialising the state value
-  state = { modalIsOpen: false };
+  state = {
+    modalIsOpen: false,
+    showBlock: false,
+  };
 
   // Function to set state value
   showModal = () => {
@@ -23,11 +27,75 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+
+        <button
+          className="Button"
+          onClick={() => {
+            this.setState((prevState) => ({
+              showBlock: !prevState.showBlock,
+            }));
+          }}
+        >
+          Toggle
+        </button>
+        <br />
+
+        {/*
+        The <Transition> component will describe a transition
+        from one component state to another over time with a simple declarative API.
+        Most commonly it's used to animate the mounting and unmounting of a component,
+        but can also be used to describe in-place transition states as well.
+        
+        It does not alter the behavior of the component it renders,
+        it only tracks "enter" and "exit" states for the components.
+        It's up to you to give meaning and effect to those states.
+        For example we can add styles to a component when it enters or exits.
+
+        There are 4 main states a Transition can be in:
+          a) 'entering'
+          b) 'entered'
+          c) 'exiting'
+          d) 'exited'
+
+        in - Transition state is toggled via the 'in' prop which shows the component; triggers the enter or exit states
+        
+        When true the component begins the "Enter" stage. 
+        During this stage, the component will shift from its current transition state,
+        to 'entering' for the duration of the transition and then to the 'entered' stage once it's complete.
+
+        timeout - The duration of the transition, in milliseconds. Required unless addEndListener is provided.
+          timeout: number | { enter?: number, exit?: number, appear?: number }
+
+        mountOnEnter - By default the child component is mounted immediately along with the parent Transition component.
+          If you want to "lazy mount" the component on the first in={true} you can set mountOnEnter.
+          After the first enter transition the component will stay mounted, even on "exited", unless you also specify unmountOnExit.
+
+        unmountOnExit - By default the child component stays mounted after it reaches the 'exited' state.
+          Set unmountOnExit if you'd prefer to unmount the component after it finishes exiting.
+        */}
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                // exiting is the state of the component in the Transition Component.
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            />
+          )}
+        </Transition>
+
         {this.state.modalIsOpen ? (
-          <Modal
-            show={this.state.modalIsOpen}
-            closed={this.closeModal}
-          />
+          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         ) : null}
         {this.state.modalIsOpen ? (
           <Backdrop show={this.state.modalIsOpen} />
