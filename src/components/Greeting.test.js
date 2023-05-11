98d57c6a207026019308d348f62569c277bbca8d
Immanual <immanualatrinity@gmail.com>
2023-05-11 07:57:50 +0530

    Grouping_Tests_Together_With_Test_Suites

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import Greeting  from './Greeting';
 
-test('renders Hello World as a text', () => {
-  // Arrange - Set up the test data, test conditions, and test environment. 
-  render(<Greeting />);
+// Test Suite
+describe('Greeting component', () => {
 
-  // Act - Run logic that should be tested.
-  // ... nothing
-  
-  // Assert - Compare execution result with expected results.
-  const helloWorldElement = screen.getByText('Hello World!');
-  expect(helloWorldElement).toBeInTheDocument();
+  // Test Spec
+  test("renders Hello World as a text", () => {
+    // Arrange - Set up the test data, test conditions, and test environment.
+    render(<Greeting />);
+
+    // Act - Run logic that should be tested.
+    // ... nothing
+
+    // Assert - Compare execution result with expected results.
+    const helloWorldElement = screen.getByText("Hello World!");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
