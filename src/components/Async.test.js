6ab7415bbc6a206ab3f8c39f679dd0b9602718dc
Immanual <immanualatrinity@gmail.com>
2023-05-13 08:48:55 +0530

    Working with mocks

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -4,6 +4,14 @@ import Async from "./Async";
 describe('Async Component', () => {
 
   test('renders posts if request succeeds', async () => {
+    // Creating mock function for the fetch api
+    window.fetch = jest.fn();
+
+    // Fetching the resolved return value from async fetch mock function
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
+
     render(<Async />);
     
     // find function returns promise if the element is on the virtual dom.
