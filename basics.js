be504c536b1ac37ea63466dd312edc2c5dd9938b
Immanual <immanualatrinity@gmail.com>
2023-05-15 09:26:53 +0530

    Working_with_Array_And_Object_Types

---
+++ basics.js
@@ -0,0 +1,27 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+// Primitives
+var age;
+age = 12;
+var userName;
+userName = "Max";
+var isInstructor;
+isInstructor = true;
+// More complex types
+var hobbies; // Type definition for array
+hobbies = ["Sports", "Cooking"];
+// Type definition of object
+var person;
+person = {
+    name: 'Max',
+    age: 32
+};
+// Type definition for array of objects
+var people;
+people = [
+    {
+        name: "Max",
+        age: 32,
+    }
+];
--- basics.ts
+++ basics.ts
@@ -7,7 +7,35 @@ let age: number;
 age = 12;
 
 let userName: string;
-userName = 'Max';
+userName = "Max";
 
 let isInstructor: boolean;
 isInstructor = true;
+
+// More complex types
+let hobbies: string[]; // Type definition for array
+hobbies = ["Sports", "Cooking"];
+
+// Type definition of object
+let person: {
+  name: string;
+  age: number;
+};
+person = {
+  name: 'Max',
+  age: 32
+};
+
+// Type definition for array of objects
+let people: {
+  name: string;
+  age: number;
+}[];
+people = [
+  {
+    name: "Max",
+    age: 32,
+  }
+];
+
+let abc: any;
