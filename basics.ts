a43ca4d3fd81805e3a9cf963d024d82c5e925514
Immanual <immanualatrinity@gmail.com>
2023-05-16 10:07:31 +0530

    Typescript generics

--- basics.ts
+++ basics.ts
@@ -22,8 +22,8 @@ let person: {
   age: number;
 };
 person = {
-  name: 'Max',
-  age: 32
+  name: "Max",
+  age: 32,
 };
 
 // Type definition for array of objects
@@ -35,18 +35,18 @@ people = [
   {
     name: "Max",
     age: 32,
-  }
+  },
 ];
 
 let abc: any;
 
 // Union type
-let a: (string | number);
-a = 'abc';
+let a: string | number;
+a = "abc";
 a = 1234;
 
 // Union type
-let x: (number | number[]);
+let x: number | number[];
 x = [1, 4, 7];
 x = 7;
 
@@ -67,14 +67,37 @@ let car: Car;
 let carArray: Car[];
 
 // Functions & types
-function sum (a: number, b: number): number {
+function sum(a: number, b: number): number {
   return a + b;
 }
 
-function add (a: number, b: number) {
+function add(a: number, b: number) {
   return a + b;
 }
 
-function print (value: any) {
+function printt(value: any) {
   console.log(value);
 }
+
+/*
+Generics - Generics offer a way to create reusable components.
+Generics provide a way to make components work with any data type and not restrict to one data type.
+So, components can be called or used with a variety of data types. Generics in TypeScript is almost similar to C# generics.
+
+TypeScript introduced generics.
+Generics uses the type variable <T>, a special kind of variable that denotes types.
+The type variable remembers the type that the user provides and works with that particular type only.
+This is called preserving the type information.
+Generics makes it easier to write reusable code.
+*/
+function insertAtBeginning<T> (array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1);
+const stringArray = insertAtBeginning(["a", "b", "c"], "e");
+
+// updatedArray[0].split('');
