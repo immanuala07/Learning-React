cd0a1e820468da9d8ac8159e025527c1c2fe00be
Immanual <immanualatrinity@gmail.com>
2023-05-20 21:04:48 +0530

    Managing_State_And_TypeScript

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/Todo';
 
 function App () {
-  const todos = [
-    new Todo("Learn React"),
-    new Todo("Learn Redux"),
-    new Todo("Learn TypeScript")
-  ];
+  /*
+  Without using the Generic in the useState hook,
+  todos variable will be of TypeScript never datatype
+  So we use the TypeScript Generic,
+  to make the TypeScript knows that todo vairable holds the values of Todo class object values in TypeScript.
+  */
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodoHandler = (todoText: string) => {
-    console.log(todoText);
+    // Creating a new todo using the todo text entered by user.
+    const newTodo = new Todo(todoText);
+    
+    setTodos((prevTodos) => {
+      // Creating a array by merging both previous todos and current todo
+      return prevTodos.concat(newTodo);
+    })
   };
 
   return (
