8ca8c84a4943a129fe99d04d4e5b326e2788e782
Immanual <immanualatrinity@gmail.com>
2023-05-26 23:49:26 +0530

    Added error modal

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,18 +2,20 @@ import React, { useCallback, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const addIngredientHandler = (ingredient) => {
     setIsLoading(true);
     fetch("https://fir-project-a6274-default-rtdb.firebaseio.com/Demo-project.json", {
       method: 'POST',
       body: JSON.stringify(ingredient),
-      headers:{'Content-Type':'application/json'}
+      headers: { 'Content-Type': 'application/json' }
     }).then((response) => {
       setIsLoading(false);
       return response.json();
@@ -25,23 +27,27 @@ function Ingredients() {
           ...ingredient,
         },
       ]);
-    })
-    
+    });    
   };
 
   const removeIngredientHandler = (id) => {
     setIsLoading(true);
     fetch(
-      `https://fir-project-a6274-default-rtdb.firebaseio.com/Demo-project/${id}.json`,
+      `https://fir-project-a6274-default-rtdb.firebaseio.com/Demo-project/${id}.jon`,
       {
         method: "DELETE",
       },
-    ).then((response) => {
-      setIsLoading(false);
-      setUserIngredients((prevIngredients) =>
-        prevIngredients.filter((ingredient) => id !== ingredient.id),
-      );
-    });
+    )
+      .then((response) => {
+        setIsLoading(false);
+        setUserIngredients((prevIngredients) =>
+          prevIngredients.filter((ingredient) => id !== ingredient.id),
+        );
+      })
+      .catch((error) => {
+        setError("Somthing went wrong !");
+        setIsLoading(false);
+      });
   };
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
@@ -49,8 +55,14 @@ function Ingredients() {
     // Since setUserIngredients is usestate updating function so it wont changes so we will not add it as dependency
   }, []);
 
+  const clearError = () => {
+    setError(null);
+  };
+
   return (
     <div className="App">
+      {error && <ErrorModal onClose={clearError} >{error}</ErrorModal>}
+
       <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
 
       <section>
