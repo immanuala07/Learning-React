48ed916b23ec242cba7b872f6c2f2f1b266b76ae
Immanual <immanualatrinity@gmail.com>
2023-05-27 13:41:39 +0530

    adding useReducer()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,12 +1,25 @@
-import React, { useCallback, useState } from "react";
+import React, { useReducer, useCallback, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
+const IngredientReducer = (currentIngredient, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredient;
+    case "ADD":
+      return [...currentIngredient, action.ingredient];
+    case "DELETE":
+      return currentIngredient.filter((ing) => ing.id !== action.id);
+    default:
+      throw new Error("Should not get there!");
+  }
+};
+
 function Ingredients() {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(IngredientReducer, []);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
@@ -20,29 +33,27 @@ function Ingredients() {
       setIsLoading(false);
       return response.json();
     }).then((responseData) => {
-      setUserIngredients((prevIngredients) => [
-        ...prevIngredients,
-        {
+      dispatch({
+        type: "ADD",
+        ingredient: {
           id: responseData.name,
           ...ingredient,
         },
-      ]);
+      });
     });    
   };
 
-  const removeIngredientHandler = (id) => {
+  const removeIngredientHandler = (ingredientId) => {
     setIsLoading(true);
     fetch(
-      `https://fir-project-a6274-default-rtdb.firebaseio.com/Demo-project/${id}.jon`,
+      `https://fir-project-a6274-default-rtdb.firebaseio.com/Demo-project/${ingredientId}.json`,
       {
         method: "DELETE",
       },
     )
       .then((response) => {
         setIsLoading(false);
-        setUserIngredients((prevIngredients) =>
-          prevIngredients.filter((ingredient) => id !== ingredient.id),
-        );
+        dispatch({ type: "DELETE", id: ingredientId });
       })
       .catch((error) => {
         setError("Somthing went wrong !");
@@ -51,7 +62,7 @@ function Ingredients() {
   };
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    setUserIngredients(filteredIngredients);
+    dispatch({ type: "SET", ingredient: filteredIngredients });
     // Since setUserIngredients is usestate updating function so it wont changes so we will not add it as dependency
   }, []);
 
