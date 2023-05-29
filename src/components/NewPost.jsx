096a70f1aa1ebee2e0e518d022c6cdf51abd4ec3
Immanual <immanualatrinity@gmail.com>
2023-05-29 10:08:26 +0530

    Lifting State Up

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -3,21 +3,25 @@ import classes from './NewPost.module.css';
 
 function NewPost () {
   const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
 
-  const changeBodyHandler = (event) => {
+  const bodyChangeHandler = (event) => {
     setEnteredBody(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value);
   };
 
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,20 +2,34 @@ import Post from './Post.jsx';
 import NewPost from './NewPost.jsx';
 import classes from "./PostsList.module.css";
 
-function PostsList(props) {
+function PostsList (props) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  const bodyChangeHandler = (event) => {
+    setEnteredBody(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value);
+  };
+
   return (
     <>
-    <NewPost/>
-    <ul className={classes.posts}>
-      <Post
-        author="Immanual"
-        body="React.js is awesome!"
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
       />
-      <Post
-        author="Imman"
-        body="Check out the full course!"
-      />
-    </ul>
+      <ul className={classes.posts}>
+        <Post
+          author={enteredAuthor}
+          body={enteredBody}
+        />
+        <Post
+          author="Imman"
+          body="Check out the full course!"
+        />
+      </ul>
     </>
   );
 }
