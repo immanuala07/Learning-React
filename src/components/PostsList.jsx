ddbd3fcd041f59f6777d4667c3da039c6cdb731d
Immanual <immanualatrinity@gmail.com>
2023-05-30 23:04:46 +0530

    Handle Loading State & adding loading content before the data is loaded

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,12 +6,15 @@ import { useEffect, useState } from 'react';
 
 function PostsList ({isPosting, onStopPosting}) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts () {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -38,7 +41,7 @@ function PostsList ({isPosting, onStopPosting}) {
         </Modal>
       )}
 
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post
@@ -50,10 +53,16 @@ function PostsList ({isPosting, onStopPosting}) {
         </ul>
       )}
 
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
