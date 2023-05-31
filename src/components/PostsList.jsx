ecc5f378c49bd01daba657f23e8a4204d81038d4
Immanual <immanualatrinity@gmail.com>
2023-05-31 10:05:45 +0530

    Data Fetching via loader()s

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,24 +1,12 @@
+import { useLoaderData } from 'react-router-dom';
 import Post from './Post';
-import NewPost from '../routes/NewPost';
 import classes from "./PostsList.module.css";
 import { useEffect, useState } from 'react';
 
 function PostsList () {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
 
-  useEffect(() => {
-    async function fetchPosts () {
-      setIsFetching(true);
-      const response = await fetch("http://localhost:8080/posts");
-      const resData = await response.json();
-      setPosts(resData.posts);
-      setIsFetching(false);
-    }
+  const posts = useLoaderData();
 
-    fetchPosts();
-  }, []);
-  
   function addPostHandler (postData) {
     fetch("http://localhost:8080/posts", {
       method: "POST",
@@ -31,7 +19,7 @@ function PostsList () {
 
   return (
     <>
-      {!isFetching && posts.length > 0 && (
+      {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post
@@ -43,16 +31,10 @@ function PostsList () {
         </ul>
       )}
 
-      {!isFetching && posts.length === 0 && (
+      {posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
-        </div>
-      )}
-
-      {isFetching && (
-        <div style={{ textAlign: "center", color: "white" }}>
-          <p>Loading posts...</p>
         </div>
       )}
     </>
