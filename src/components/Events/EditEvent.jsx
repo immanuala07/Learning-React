94ad04b7419e51ea8bbaaedfb984693cca8841eb
Immanual <immanualatrinity@gmail.com>
2023-09-24 13:28:14 +0530

    React Query And React Router

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,96 +1,97 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, redirect, useNavigate, useNavigation, useParams, useSubmit } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { fetchEvent, updateEvent, queryClient } from "../../util/http.js";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { state } = useNavigation();
+  const submit = useSubmit();
   const params = useParams();
 
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isError, error } = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+    staleTime: 10000
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    /**
-     * onMutate is an optional callback function you can provide when using the useMutation hook in React Query.
-     * This callback function is executed before the actual mutation occurs
-     * and allows you to perform actions such as optimistic updates or other custom logic
-     * before the mutation request is sent to the server.
-     */
-    onMutate: (data) => {
-      const newEvent = data.event;
-      /**
-       * cancelQueries function provided by the queryClient instance to cancel one or more ongoing queries.
-       * This is useful to stop the execution of queries in response to user actions,
-       * such as navigating away from a component or when a new request supersedes a previous one.
-       */
-      queryClient.cancelQueries({ queryKey: ["events", params.id] });
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   /**
+  //    * onMutate is an optional callback function you can provide when using the useMutation hook in React Query.
+  //    * This callback function is executed before the actual mutation occurs
+  //    * and allows you to perform actions such as optimistic updates or other custom logic
+  //    * before the mutation request is sent to the server.
+  //    */
+  //   onMutate: (data) => {
+  //     const newEvent = data.event;
+  //     /**
+  //      * cancelQueries function provided by the queryClient instance to cancel one or more ongoing queries.
+  //      * This is useful to stop the execution of queries in response to user actions,
+  //      * such as navigating away from a component or when a new request supersedes a previous one.
+  //      */
+  //     queryClient.cancelQueries({ queryKey: ["events", params.id] });
 
-      /**
-       * getQueryData method provided by the queryClient instance to
-       * access the cached query data outside of a component,
-       * by specifying the query key associated with the data we want to retrieve.
-       */
-      const previousEvent = queryClient.getQueryData(["events", params.id]);
+  //     /**
+  //      * getQueryData method provided by the queryClient instance to
+  //      * access the cached query data outside of a component,
+  //      * by specifying the query key associated with the data we want to retrieve.
+  //      */
+  //     const previousEvent = queryClient.getQueryData(["events", params.id]);
 
-      /**
-       * The below method provided by the queryClient instance to manually update the data in the query cache.
-       * This can be useful for implementing custom data updates or optimizations.
-       */
-      queryClient.setQueryData(["events", params.id], newEvent);
+  //     /**
+  //      * The below method provided by the queryClient instance to manually update the data in the query cache.
+  //      * This can be useful for implementing custom data updates or optimizations.
+  //      */
+  //     queryClient.setQueryData(["events", params.id], newEvent);
 
-      // Returns context containing data to use for rolling back.
-      return { previousEvent };
-    },
-    /**
-     * onError option allows you to specify a callback function that is called when a mutation encounters an error.
-     * This can be useful for handling and responding to errors in a customized way,
-     * such as displaying error messages or performing specific actions when a mutation fails.
-     *
-     * context - The data or object return from onMutate function.
-     */
-    onError: (error, data, context) => {
-      // The below method provided by the queryClient instance to manually update the data in the query cache.
-      // This can be useful for implementing custom data updates or optimizations.
-      queryClient.setQueryData(["events", params.id], context.previousEvent);
-    },
-    /**
-     * useMutation hook that allows you to specify a callback function to be called after a mutation has settled,
-     * regardless of whether it succeeded or failed.
-     * This can be useful for performing actions such as cleaning up after a mutation,
-     * displaying a toast message, or triggering additional queries.
-     *
-     * It receives four arguments:
-     * 1) data: The data returned by the mutation function if it succeeded.
-     * 2) error: The error object if the mutation failed (or null if it succeeded).
-     * 3) variables: The variables passed to the mutation function.
-     * 4) context: The context passed to the mutation function (if provided)
-     */
-    onSettled: () => {
-      /**
-       * By using the below statement,
-       * it tells React Query that the data fetched by certain queries is outdated now,
-       * and it should be marked as stale so that an immediate refetch should be triggered,
-       * if the Query belongs to a component that's currently visible on the screen.
-       *
-       * invalidateQueries takes an object as a parameter where we define the Query key to target in the same way as it is used in queries.
-       * Note: Query key doesn't have to be exactly the same key. So, all the queries with that key pattern will be invalidated and immediately refetch/queries will be triggered.
-       */
-      queryClient.invalidateQueries(["events", params.id]);
-    },
-  });
+  //     // Returns context containing data to use for rolling back.
+  //     return { previousEvent };
+  //   },
+  //   /**
+  //    * onError option allows you to specify a callback function that is called when a mutation encounters an error.
+  //    * This can be useful for handling and responding to errors in a customized way,
+  //    * such as displaying error messages or performing specific actions when a mutation fails.
+  //    *
+  //    * context - The data or object return from onMutate function.
+  //    */
+  //   onError: (error, data, context) => {
+  //     // The below method provided by the queryClient instance to manually update the data in the query cache.
+  //     // This can be useful for implementing custom data updates or optimizations.
+  //     queryClient.setQueryData(["events", params.id], context.previousEvent);
+  //   },
+  //   /**
+  //    * useMutation hook that allows you to specify a callback function to be called after a mutation has settled,
+  //    * regardless of whether it succeeded or failed.
+  //    * This can be useful for performing actions such as cleaning up after a mutation,
+  //    * displaying a toast message, or triggering additional queries.
+  //    *
+  //    * It receives four arguments:
+  //    * 1) data: The data returned by the mutation function if it succeeded.
+  //    * 2) error: The error object if the mutation failed (or null if it succeeded).
+  //    * 3) variables: The variables passed to the mutation function.
+  //    * 4) context: The context passed to the mutation function (if provided)
+  //    */
+  //   onSettled: () => {
+  //     /**
+  //      * By using the below statement,
+  //      * it tells React Query that the data fetched by certain queries is outdated now,
+  //      * and it should be marked as stale so that an immediate refetch should be triggered,
+  //      * if the Query belongs to a component that's currently visible on the screen.
+  //      *
+  //      * invalidateQueries takes an object as a parameter where we define the Query key to target in the same way as it is used in queries.
+  //      * Note: Query key doesn't have to be exactly the same key. So, all the queries with that key pattern will be invalidated and immediately refetch/queries will be triggered.
+  //      */
+  //     queryClient.invalidateQueries(["events", params.id]);
+  //   },
+  // });
 
   function handleSubmit (formData) {
-    mutate({ id: params.id, event: formData });
-    navigate('../');
+    submit(formData, { method: 'PUT' });
   }
 
   function handleClose() {
@@ -98,14 +99,6 @@ export default function EditEvent() {
   }
 
   let content;
-
-  if (isPending) {
-    content = (
-      <div className="center">
-        <LoadingIndicator />
-      </div>
-    );
-  }
 
   if (isError) {
     content = (
@@ -129,12 +122,18 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
+        {state === "submitting" ? (
+          <p>Sending data....</p>
+        ) : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
     );
   }
@@ -144,4 +143,19 @@ export default function EditEvent() {
       {content}
     </Modal>
   );
+}
+
+export function loader ({params}) {
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id })
+  });
+}
+
+export async function action ({request,params}) {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+  await updateEvent({ id: params.id, event: updatedEventData });
+  await queryClient.invalidateQueries(['events']);
+  return redirect('../');
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
-export default function Header({ children }) {
+import { useIsFetching } from "@tanstack/react-query";
+
+export default function Header ({ children }) {
+  /**
+   * useIsFetching is a hook provided by React Query that allows us
+   * to track the number of ongoing queries or mutations in your application.
+   * It's particularly useful when we want to display loading indicators
+   * or perform certain actions based on the global loading state of your queries and mutations.
+   *
+   * If 'fetching' is greater than 0,
+   * we can display a loading indicator to inform the user that
+   * there are ongoing network requests.
+   */
+  const fetching = useIsFetching();
+
   return (
     <>
-      <div id="main-header-loading"></div>
+      <div id="main-header-loading">{fetching > 0 && <progress />}</div>
       <header id="main-header">
         <div id="header-title">
           <h1>React Events</h1>
